944efa04254e77e52edc40d725d8294da6b86e4b
beasiteri <siteribea@gmail.com>
2022-05-31 16:57:14 +0200

    Modify mysql query

--- modules/events.js
+++ modules/events.js
@@ -2,18 +2,24 @@ const mysql = require('mysql');
 const connection = require('./sql-connection');
 
 async function getAllEvents(req, res) {
-  let sql = `SELECT * FROM event INNER JOIN city
-  ON event.city = city.pk
-  INNER JOIN country
-  ON city.country = country.pk;`;
-  connection.query(sql, (error, results) => {
-    if (error) {
-      return console.error(error.message);
-    }
-    res.send(results);
-  });
+  try {
+    let sql = `SELECT event.name AS event_name, 
+      description, address, date,
+      city.name AS city_name, 
+      country.name AS country_name FROM event INNER JOIN city
+      ON event.city = city.pk
+      INNER JOIN country
+      ON city.country = country.pk;`;
 
-  connection.end();
+    connection.query(sql, (err, results) => {
+      res.send(results);
+    });
+  } catch (err) {
+    console.log('An error happened in function getAllEvents: ', err.message);
+    res.end(err.message);
+  } finally {
+    connection.end();
+  }
 }
 
 module.exports = {
